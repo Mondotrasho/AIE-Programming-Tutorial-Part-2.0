502b481ff7a20368bbc840b208ce110bca5f0eeb
Mondotrasho <oscar@peoplepictures.com.au>
2018-02-06 15:07:20 +1100

    Counts odd numbers by adding 1 by 1

---
+++ Loop count odd numbers.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+int main()
+{
+	int total = 0;
+	int count = 0;
+	int number = 1;
+
+	while (count < 15) {
+		total += number;
+		number += 2;
+		count += 1;
+	}
+
+	std::cout << "Total of the first 15 odd numbers is " << total << std::endl;
+
+	system("pause");
+	
+	return 0;
+
+}
